3aaea4210bdee6907bae75bc2b62bdcdbd1cafbf
Will Underwood <wwunderwood93@gmail.com>
2017-09-25 20:55:24 -0400

    Moved all my files to the correct workspace. Fixed errors

---
+++ IMS/src/main/webapp/js/cust-show-info.js
@@ -0,0 +1,20 @@
+
+var app = angular.module('custShowInfoApp', []);
+
+app.controller('custShowInfoController', function($scope) {
+	$scope.custInfo = {
+		firstName: "Max",
+		lastName: "Caulfield",
+		email: "mcaulfi1@blackwell.com",
+		address: "123 Deer St",
+		city: "Arcadia Bay",
+		state: "Oregon",
+		zipcode: "123456",
+		phone: "123-456-7890"
+	}
+	
+	$scope.showModal = false;
+    $scope.open = function(){
+    $scope.showModal = !$scope.showModal;
+    };
+});
