dc9b47dd2188aec57c00489dd11660317a50505d
Matthew Prass <mattprass@gmail.com>
2017-09-28 00:31:53 -0400

    added some new query methods

--- IMS/src/main/java/com/ims/repositories/CustomerRepository.java
+++ IMS/src/main/java/com/ims/repositories/CustomerRepository.java
@@ -4,9 +4,19 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ims.beans.Card;
 import com.ims.beans.Customer;
+import com.ims.beans.State;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 
 	List<Customer> findBylastname(String lastname);
+	
+	Customer findByEmailAndPassword(String email, String password);
+	
+	List<Customer> findByState(State state);
+	
+	Customer findByCard(Card card);
+	
+	Customer findByCardId(int cardid);
 }
