2c30ffdb17be72716236341d0c67b8ee85c2fbb8
Will Underwood <wwunderwood93@gmail.com>
2017-09-28 14:06:02 -0400

    Routing.
    Fixed cart button. It now routes to the cart.

--- IMS/src/main/webapp/JS/home.js
+++ IMS/src/main/webapp/JS/home.js
@@ -13,6 +13,45 @@ storeApp.config(function($stateProvider, $urlRouterProvider) {
 	$stateProvider
 		.state("mainStorePage", {
 			url:"/mainStorePage",
-			templateUrl: "partials/mainStorePage.html", //html
+			templateUrl: "partials/mainStorePage.html"
+		})
+		.state("cart", {
+			url: "/cart",
+			templateUrl: "partials/cust-cart.html"
 		});
+});
+
+
+storeApp.controller('cartController', function($scope) {
+	$scope.items = [
+		{
+			name: "Blue Beanie",
+			price: 9.99,
+			quantity: 1
+		},
+		{
+			name: "Candlemass T-Shirt",
+			price: 19.99,
+			quantity: 1
+		},
+		{
+			name: "Black Denim Jacket",
+			price: 39.99,
+			quantity: 1
+		},
+		{
+			name: "Raven Feather Necklace",
+			price: 9.99,
+			quantity: 1
+		}
+	]
+	
+	$scope.getTotal = function() {
+		var total = 0;
+		for(var i = 0; i < $scope.items.length; i++) {
+	        var product = $scope.items[i];
+	        total += (product.price * product.quantity);
+	    }
+	    return total;
+	}
 });
