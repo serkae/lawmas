2b43113139430e04eb2b9a0f78531b8314529aba
Matthew Prass <mattprass@gmail.com>
2017-09-28 19:18:50 -0400

    PAPA BLESS

--- IMS/src/main/java/com/ims/controllers/InventoryItemController.java
+++ IMS/src/main/java/com/ims/controllers/InventoryItemController.java
@@ -1,6 +1,7 @@
 package com.ims.controllers;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.ims.beans.InventoryItem;
@@ -69,19 +70,20 @@ public class InventoryItemController {
 	
 	@RequestMapping(value = "/upload", method = (RequestMethod.POST))
 	public ResponseEntity<String> handlePictureUpload(
-			@RequestParam("name") String name,
-			@RequestParam("filepath") String filepath) {
+			@RequestParam("file") MultipartFile f) {
 		
 		BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAI3MXOKNTE6DFBHAA", "SwxW8kCo0VhbxDLkS2i6n1Zn1vriO2g0rSv1o2bX");
         AmazonS3 client = AmazonS3ClientBuilder.standard()
                                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                                 .withRegion(Regions.US_EAST_1)
                                 .build();
+        File file = new File(f.getOriginalFilename());
+        
 		try {
-            System.out.println("Uploading a new object to S3 from a file\n");
-            File f = new File(filepath);
+			f.transferTo(file);
+            System.out.println("Uploading a new object to S3 from a file:"+f.getName()+"\n");
             client.putObject(new PutObjectRequest(
-            		                 bucketname, name, f));
+            		                 bucketname, f.getOriginalFilename(), file));
 
            
          } catch (AmazonServiceException ase) {
@@ -101,10 +103,18 @@ public class InventoryItemController {
                     "communicate with S3, " +
                     "such as not being able to access the network." +
            "Error Message: " + ace.getMessage())));
-        }
+        } catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		
-		return new ResponseEntity<String>("https://s3.amazonaws.com/"+bucketname+"/"+name, HttpStatus.OK);
+		System.out.println("file link: https://s3.amazonaws.com/"+bucketname+"/"+f.getOriginalFilename());
+		ResponseEntity<String> r = new ResponseEntity<String>("https://s3.amazonaws.com/"+bucketname+"/"+f.getOriginalFilename(),HttpStatus.OK);
+		System.out.println(r.getBody());
+		return new ResponseEntity<String>("https://s3.amazonaws.com/"+bucketname+"/"+f.getOriginalFilename(), HttpStatus.OK);
 		
 		
 	}
